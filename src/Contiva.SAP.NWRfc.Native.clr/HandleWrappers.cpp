c073a823126e1a699c32b099151db64aab36ce05
Frank Wagner <f.wagner@wagnersolutions.de>
2018-10-18 20:41:22 +0200

    added rc in dispose to read it in debugger

--- src/Contiva.SAP.NWRfc.Native.clr/HandleWrappers.cpp
+++ src/Contiva.SAP.NWRfc.Native.clr/HandleWrappers.cpp
@@ -16,28 +16,28 @@ namespace Contiva {
 
 				void FunctionDescriptionHandle::DestroyHandle(RFC_FUNCTION_DESC_HANDLE handle)
 				{
-					RfcDestroyFunctionDesc(handle, NULL);
+					RFC_RC rc = RfcDestroyFunctionDesc(handle, NULL);
 				}
 
 				void TypeDescriptionHandle::DestroyHandle(RFC_TYPE_DESC_HANDLE handle)
 				{
-					RfcDestroyTypeDesc(handle, NULL);
+					RFC_RC rc = RfcDestroyTypeDesc(handle, NULL);
 				}
 
 				void FunctionHandle::DestroyHandle(RFC_FUNCTION_HANDLE handle)
 				{
-					RfcDestroyFunction(handle, NULL);
+					RFC_RC rc = RfcDestroyFunction(handle, NULL);
 				}
 
 				void StructureHandle::DestroyHandle(RFC_STRUCTURE_HANDLE handle)
 				{
-					RfcDestroyStructure(handle, NULL);
+					RFC_RC rc = RfcDestroyStructure(handle, NULL);
 				}
 
 				void TableHandle::DestroyHandle(RFC_TABLE_HANDLE handle)
 				{
 
-					RfcDestroyTable(handle, NULL);
+					RFC_RC rc = RfcDestroyTable(handle, NULL);
 				}
 			}
 		}
