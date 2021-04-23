fb87426c0ea8e5d660901e12de370d76466d0818
Frank Wagner <frank2568@gmail.com>
2021-04-23 22:10:31 +0200

    table iterator: don't  read row if MoveToFirstTableRow fails

--- src/YaNco.Core/TableRowEnumerator.cs
+++ src/YaNco.Core/TableRowEnumerator.cs
@@ -53,9 +53,12 @@ namespace Dbosoft.YaNco
 
         public void Reset()
         {
-            _handle.IfSome(h => _rfcRuntime.MoveToFirstTableRow(h));
             _first = true;
-            ReadCurrentRow();
+
+            _handle.IfSome(h =>
+            {
+                _rfcRuntime.MoveToFirstTableRow(h).Map(u => ReadCurrentRow());
+            });
         }
         
         public Structure Current
