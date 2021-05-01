d39752123117b7845ecd62186ea5150fef439c12
Frank Wagner <frank2568@gmail.com>
2021-05-01 02:20:47 +0200

    calling functions and commit

---
+++ test/YaNco.Core.Tests/RfcMock/ConnectionMockExtensions.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dbosoft.YaNco;
+using LanguageExt;
+using Moq;
+
+namespace YaNco.Core.Tests.RfcMock
+{
+    public static class ConnectionMockExtensions
+    {
+        public static async Task<IConnection> CreateConnection(this Mock<IRfcRuntime> mock)
+        {
+            return await Connection.Create(new Dictionary<string, string>(),
+                    mock.Object)
+                .IfLeft(l => throw new Exception(l.Message));
+
+        }
+
+        public static Mock<IRfcRuntime> SetupOpenConnection(this Mock<IRfcRuntime> mock, out Mock<IConnectionHandle> handle)
+        {
+            handle = new Mock<IConnectionHandle>();
+
+            mock.Setup(x =>
+                    x.OpenConnection(new Dictionary<string, string>()))
+                .Returns(Prelude.Right(handle.Object));
+
+            return mock;
+        }
+
+        public static Mock<IRfcRuntime> SetupGetFunctionDescription(this Mock<IRfcRuntime> mock, string functionName, out Mock<IFunctionDescriptionHandle> handle)
+        {
+            handle = new Mock<IFunctionDescriptionHandle>();
+
+            mock.Setup(x =>
+                    x.GetFunctionDescription(
+                        It.IsAny<IConnectionHandle>(),
+                        functionName))
+                .Returns(Prelude.Right(handle.Object));
+
+            return mock;
+        }
+
+        public static Mock<IRfcRuntime> SetupGetFunction(this Mock<IRfcRuntime> mock, Mock<IFunctionDescriptionHandle> descHandle, out Mock<IFunctionHandle> handle)
+        {
+            handle = new Mock<IFunctionHandle>();
+
+            mock.Setup(x =>
+                    x.CreateFunction(
+                        descHandle.Object))
+                .Returns(Prelude.Right(handle.Object));
+
+
+            return mock;
+        }        
+        
+        public static Mock<IRfcRuntime> SetupFunction(this Mock<IRfcRuntime> mock, string functionName, Mock<IConnectionHandle> connHandle, Action<Mock<IRfcRuntime>, IFunctionHandle> functionBuilder)
+        {
+            mock.SetupGetFunctionDescription(functionName, out var descHandle)
+                .SetupGetFunction(descHandle, out var funcHandle);
+
+            mock.Setup(x =>
+                    x.Invoke(connHandle.Object, funcHandle.Object))
+                .Returns(Prelude.Right(Unit.Default));
+
+            functionBuilder(mock,funcHandle.Object);
+
+            return mock;
+        }
+    }
+}
