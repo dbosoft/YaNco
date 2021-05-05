af9ebf29798c2f4535471e4418c851b0f2f2c42d
Frank Wagner <frank2568@gmail.com>
2021-05-05 23:45:08 +0200

    aspnet example with net472

--- samples/netcore2.1/SAPWebAPI/Program.cs
+++ samples/netcore2.1/SAPWebAPI/Program.cs
@@ -1,13 +1,5 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using WebApi;
 
 namespace SAPWebAPI
