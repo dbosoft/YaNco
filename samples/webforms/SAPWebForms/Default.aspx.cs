352cc377f6558afdebf16b49d50bdc000df194c0
Frank Wagner <frank2568@gmail.com>
2021-05-07 16:43:22 +0200

    sample with webforms

---
+++ samples/webforms/SAPWebForms/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Dbosoft.YaNco;
+
+namespace SAPWebForms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var settings = new Dictionary<string, string>
+            {
+
+            };
+
+
+            var connectionBuilder = new ConnectionBuilder(settings);
+
+            using (var context = new RfcContext(connectionBuilder.Build()))
+            {
+                var response = context.PingAsync().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
---
+++ samples/webforms/SAPWebForms/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SAPWebForms.Global" Language="C#" %>
