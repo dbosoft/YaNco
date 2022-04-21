4deec923406926adc2b628da66d42b7979831f1d
Frank Wagner <frank2568@gmail.com>
2022-04-21 09:42:46 +0200

    improved FunctionalFunctionsExtensions docs

--- src/YaNco.Core/FunctionalFunctionsExtensions.cs
+++ src/YaNco.Core/FunctionalFunctionsExtensions.cs
@@ -7,18 +7,40 @@ namespace Dbosoft.YaNco
 {
     public static class FunctionalFunctionsExtensions
     {
+        /// <summary>
+        /// Commits current transaction in ABAP backend.
+        /// This method accepts a <see cref="EitherAsync{RfcErrorInfo,R1}"/> with any right value and returns it after commit.
+        /// </summary>
+        /// <typeparam name="R1">Type of function result</typeparam>
+        /// <param name="self"></param>
+        /// <param name="context"></param>
+        /// <returns>A <see cref="EitherAsync{RfcErrorInfo,R1}"/> with the Right value of argument self or the left value.</returns>
         public static EitherAsync<RfcErrorInfo, R1> Commit<R1>(this EitherAsync<RfcErrorInfo, R1> self,
             IRfcContext context)
         {
             return self.Bind(res => context.Commit().Map(u => res));
         }
 
+        /// <summary>
+        /// Commits current transaction in ABAP backend and waits for the commit to be completed.
+        /// This method accepts a <see cref="EitherAsync{RfcErrorInfo,R1}"/> with any right value and returns it after commit.
+        /// </summary>
+        /// <typeparam name="R1">Type of function result</typeparam>
+        /// <param name="self"></param>
+        /// <param name="context"></param>
+        /// <returns>A <see cref="EitherAsync{RfcErrorInfo,R1}"/> with the Right value of argument self or the left value.</returns>
         public static EitherAsync<RfcErrorInfo, R1> CommitAndWait<R1>(this EitherAsync<RfcErrorInfo, R1> self,
             IRfcContext context)
         {
             return self.Bind(res => context.CommitAndWait().Map(u => res));
         }
 
+        /// <summary>
+        /// This methods extracts the value of a RETURN structure (BAPIRET or BAPIRET2) and processes it's value as
+        /// left value if return contains a non-successful result (abort or error). 
+        /// This method accepts a <see cref="EitherAsync{RfcErrorInfo,IFunction}"/> with <see cref="IFunction"/> as right value and returns it if return contains a successful result.
+        /// </summary>
+        /// <returns>A <see cref="EitherAsync{RfcErrorInfo,IFunction}"/> with the function as right value or the left value.</returns>
         public static EitherAsync<RfcErrorInfo, IFunction> HandleReturn(this EitherAsync<RfcErrorInfo, IFunction> self)
         {
             return self.ToEither().Map(f => f.HandleReturn()).ToAsync();
@@ -29,6 +51,13 @@ namespace Dbosoft.YaNco
             return self.Map(_ => Unit.Default);
         }
 
+        /// <summary>
+        /// This methods extracts the value of a RETURN structure (BAPIRET or BAPIRET2) and processes it's value as
+        /// left value if return contains a non-successful result (abort or error). 
+        /// This method accepts a EitherAsync with any right value.
+        /// </summary>
+        /// <param name="self"></param>
+        /// <returns>A <see cref="EitherAsync{RfcErrorInfo,IFunction}"/> with the function as right value or the left value.</returns>
         public static Either<RfcErrorInfo, IFunction> HandleReturn(this Either<RfcErrorInfo, IFunction> self)
         {
             return self.Bind(f => (
@@ -67,7 +96,7 @@ namespace Dbosoft.YaNco
         /// <param name="Input">Input function lifted in either monad.</param>
         /// <param name="Output">Output function lifted in either monad.</param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns>Result of output mapping function.</returns>
         public static EitherAsync<RfcErrorInfo, TResult> CallFunction<TRInput, TResult>(
             this IRfcContext context, 
             string functionName, 
@@ -90,11 +119,11 @@ namespace Dbosoft.YaNco
         /// CallFunction with RfcErrorInfo lifted output.
         /// </summary>
         /// <typeparam name="TResult"></typeparam>
-        /// <param name="context"></param>
-        /// <param name="functionName"></param>
+        /// <param name="context">RFC context</param>
+        /// <param name="functionName">ABAP function name</param>
         /// <param name="Output">Output function lifted in either monad.</param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns>Result of output mapping function.</returns>
         public static EitherAsync<RfcErrorInfo, TResult> CallFunction<TResult>(
             this IRfcContext context, 
             string functionName, 
@@ -110,12 +139,12 @@ namespace Dbosoft.YaNco
         }
 
         /// <summary>
-        /// CallFunction without input or output
+        /// CallFunction without input or output.
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="functionName"></param>
+        /// <param name="context">RFC context</param>
+        /// <param name="functionName">ABAP function name</param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns>Unit</returns>
         public static EitherAsync<RfcErrorInfo, Unit> CallFunctionOneWay(
             this IRfcContext context, 
             string functionName,
@@ -129,11 +158,11 @@ namespace Dbosoft.YaNco
         /// <summary>
         /// CallFunction with RfcInfo lifted input and no output
         /// </summary>
-        /// <param name="context"></param>
-        /// <param name="functionName"></param>
+        /// <param name="context">RFC context</param>
+        /// <param name="functionName">ABAP function name</param>
         /// <param name="Input">Input function lifted in either monad.</param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
+        /// <returns>Unit</returns>
         public static EitherAsync<RfcErrorInfo, Unit> CallFunctionOneWay<TRInput>(
             this IRfcContext context, 
             string functionName, 
