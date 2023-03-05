27e45c14248debcd41da71d2dfb3a0a38dc22061
Frank Wagner <frank2568@gmail.com>
2023-03-06 00:21:17 +0100

    completly wrap RfcErrorInfo in RfcError

--- src/YaNco.Primitives/RfcError.cs
+++ src/YaNco.Primitives/RfcError.cs
@@ -1,4 +1,4 @@
-﻿using JetBrains.Annotations;
+﻿using System;
 using LanguageExt;
 using LanguageExt.Common;
 
@@ -6,6 +6,26 @@ namespace Dbosoft.YaNco;
 
 public record RfcError(RfcErrorInfo RfcErrorInfo, Option<Error> Inner = new()) : Expected(RfcErrorInfo.Message, (int)RfcErrorInfo.Code, Inner)
 {
+    public RfcRc Rc => RfcErrorInfo.Code;
+
+    public RfcErrorGroup Group => RfcErrorInfo.Group;
+
+    public string Key => RfcErrorInfo.Key;
+
+    public string AbapMsgClass => RfcErrorInfo.AbapMsgClass;
+
+    public string AbapMsgType => RfcErrorInfo.AbapMsgType;
+
+    public string AbapMsgNumber => RfcErrorInfo.AbapMsgNumber;
+
+    public string AbapMsgV1 => RfcErrorInfo.AbapMsgV1;
+
+    public string AbapMsgV2 => RfcErrorInfo.AbapMsgV2;
+
+    public string AbapMsgV3 => RfcErrorInfo.AbapMsgV3;
+
+    public string AbapMsgV4 => RfcErrorInfo.AbapMsgV4;
+
     public static RfcError Ok => RfcErrorInfo.Ok().ToRfcError();
 
     public static RfcError EmptyResult => RfcErrorInfo.EmptyResult().ToRfcError();
@@ -13,5 +33,5 @@ public record RfcError(RfcErrorInfo RfcErrorInfo, Option<Error> Inner = new()) :
     public static RfcError Error(string message, RfcRc rc = RfcRc.RFC_ILLEGAL_STATE) =>
         YaNco.RfcErrorInfo.Error(message, rc).ToRfcError();
 
-    public static RfcError Error(RfcRc rc = RfcRc.RFC_ILLEGAL_STATE) => YaNco.RfcErrorInfo.Error(rc).ToRfcError();
+    public static RfcError Error(RfcRc rc = RfcRc.RFC_ILLEGAL_STATE) => RfcErrorInfo.Error(rc).ToRfcError();
 }
