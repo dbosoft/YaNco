c983af9d83137c48aa557144b1b3ca4ab3014b90
Frank Wagner <frank2568@gmail.com>
2024-12-02 12:04:51 +0100

    Allow direct access to connection and datacontainer handles (#317)
    For calling APIs directly from the functional interfaces handles have to be used for connections and data containers. These are now also exposed on the default interfaces.

--- src/YaNco.Abstractions/IFunction.cs
+++ src/YaNco.Abstractions/IFunction.cs
@@ -4,6 +4,12 @@ namespace Dbosoft.YaNco;
 
 public interface IFunction : IDataContainer
 {
+    /// <summary>
+    /// Direct access to the function handle.
+    /// </summary>
+    /// <remarks>
+    /// Use this property only if you would like to call runtime api methods that are not covered by the <see cref="IFunction"/> interface.
+    /// </remarks>
     [Browsable(false)]
-    IFunctionHandle Handle { get; }   
+    new IFunctionHandle Handle { get; }   
 }
